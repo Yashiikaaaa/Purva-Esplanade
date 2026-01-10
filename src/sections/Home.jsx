1599820b0b52a252402208a6d989140358b18954
yashiikaaaa <yashika@truestate.in>
2026-01-10 13:32:16 +0530

    home image change

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,4 +1,4 @@
-import herobanner from "../assets/purvaesp.webp"; // Importing hero banner background image
+import herobanner from "../assets/purva-blubelle-maxresdefault.jpg"; // Importing hero banner background image
 import Button from "../components/button/buttonMain"; // Importing the Button component for reusability
 import homeLocation from "../assets/home/location.svg"; // Importing location icon
 import { useEffect, useState } from "react";
